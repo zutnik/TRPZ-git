fb7b1b1358b293674c5ed3845633e96e0ebc99fd
KorolSt <korolst43@gmail.com>
2021-09-22 10:00:09 +0300

    Added Korol name

--- hello.py
+++ hello.py
@@ -1,4 +1,20 @@
 print("Hello World")
+<<<<<<< HEAD
 
+
+=======
 print('Stepan Korol')
-
+print("FirstName LastName")
+print("Ilona Mykhailova")
+print("Serhii Basik")
+print("Artem Bystritskyi")
+print("Ivashchenko Daniil")
+print('Oleksiy Didenko')
+print('Dmytro Tymoshok')
+print("Oleksandr Kachanov")
+print('Tolkunov Ivan')
+print("Andriy Dyug")
+print("Oleksandr Drozdiuk")
+print("Anastasiia Hrynenko")
+print('Khomutov Ilya')
+>>>>>>> e85e2917f6850da31591f83f6535ed87fbb54359
