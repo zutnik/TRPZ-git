369ba055a5285e5a3d0cb19136345d807468da72
zutnik <54157558+zutnik@users.noreply.github.com>
2021-10-06 09:55:18 +0300

    Bogdan Zhytnyk

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,6 +1,5 @@
 ﻿Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
-        listBox1.Items.Add("FirstName LastName")
-        listBox1.Items.Add("Illia Pashchenko")
+        listBox1.Items.Add("Bogdan Zhytnyk")
     End Sub
 End Class
